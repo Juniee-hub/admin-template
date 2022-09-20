efbff674f33d29c778c31d8d792a545806f59c28
Juniee-hub <junes3754@gmail.com>
2022-09-20 11:48:23 +0900

    메뉴패널 추가

--- src/components/webmaster/pannels/leftPanels/leftPanel.jsx
+++ src/components/webmaster/pannels/leftPanels/leftPanel.jsx
@@ -1,23 +1,61 @@
-import React from 'react'
+import React                                 from 'react'
+import { Menu, Button }                      from 'antd'
+import { AppstoreOutlined, SettingOutlined } from '@ant-design/icons'
+import Link                                  from 'next/link'
+import { Reoverlay }                         from 'reoverlay'
 
-import styles from './leftPanel.module.css'
+import { useModal } from '../../../../hooks/useModal'
+import { getItem }      from '../../../../service/customFunction'
+import styles              from './leftPanel.module.css'
 
 const AdminLeftPanel = () => {
+
+    const items = [
+        getItem('회원', 'sub2', <AppstoreOutlined/>, [
+            getItem(<Link href={`${process.env.NEXT_PUBLIC_ADMIN_NAME}/member`}>회원 목록</Link>, '5'),
+            getItem('회원 등록', '6'),
+            getItem('회원 관리', 'sub3', null, [getItem('탈퇴 회원', '7'), getItem('정지 회원', '8')]),
+        ]),
+    ]
+
+    const onClick = (e) => {
+        console.log('click', e)
+    }
+
+    const handlePopup = () => {
+        Reoverlay.showModal(useModal, {
+            title:'테스트 모달',
+            components: '컴포넌트 자리',
+            onConfirm: () => {
+                alert('zzz')
+            }
+        })
+    }
+
     return (
-        <section className={styles.wrap}>
-            <div className={styles.logoBox}>로고 자리</div>
+        <aside className={styles.wrap}>
+            <section className={styles.logoBox}>로고 자리</section>
 
-            <div className={styles.menuWrap}>
-                <div>Menu1</div>
-                <div>Menu2</div>
-                <div>Menu3</div>
-                <div>Menu4</div>
-            </div>
+            <section className={styles.menuWrap}>
+                <Menu
+                    key={'1'}
+                    onClick={onClick}
+                    style={{ width: '7rem' }}
+                    items={items}
+                />
 
-            <div className={styles.setupWrap}>
-                <div>Setup Button</div>
-            </div>
-        </section>
+                <Menu
+                    key={'2'}
+                    onClick={onClick}
+                    style={{ width: '7rem' }}
+                    items={items}
+                />
+            </section>
+
+            <section className={styles.setupWrap}>
+                <Button type="primary" shape="circle" icon={<SettingOutlined/>} onClick={handlePopup}/>
+            </section>
+        </aside>
     )
 }
 
